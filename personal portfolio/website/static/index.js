8b8e585064b41558521daa3f5a97ecac0773b8cd
RockaDev <83962580+RockaDev@users.noreply.github.com>
2021-12-02 17:39:57 +0100

    Add files via upload

---
+++ personal portfolio/website/static/index.js
@@ -0,0 +1,13 @@
+function deleteNote(noteId) {
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({ noteId: noteId })
+
+    }).then((_res) => {
+        window.location.href= '/';
+    });
+}
+
+function accessInfo() {
+    document.getElementById("demo").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country.<br/>Good luck to you!";
+}
